f5c4e53d426488d72388c6d0b5cc403084bb2706
Evaldo de Souza <evaldocz18@gmail.com>
2021-04-28 10:02:27 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/evaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/evaldo/cursomc/services/CategoriaService.java
@@ -3,6 +3,9 @@ package com.evaldo.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -46,5 +49,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
